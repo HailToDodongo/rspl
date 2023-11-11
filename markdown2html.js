8582257a906f1bb0d8c5d034f09124eef24259ee
Max Bebök <beboek.max@gmail.com>
2023-11-11 12:42:31 +0100

    chg: tidy up HTML

--- markdown2html.js
+++ markdown2html.js
@@ -6,9 +6,4 @@ const markdown = fs.readFileSync('./Docs.md', 'utf8');
 const converter = new showdown.Converter();
 const html = converter.makeHtml(markdown);
 
-let mainHtml = fs.readFileSync('./src/web/index.html', 'utf8');
-
-const offsetStart = mainHtml.indexOf('RSPL_DOCS">') + 'RSPL_DOCS">'.length;
-const offsetEnd = mainHtml.indexOf('</div>', offsetStart);
-mainHtml = mainHtml.substring(0, offsetStart) + html + mainHtml.substring(offsetEnd);
-fs.writeFileSync('./src/web/index.html', mainHtml);
+fs.writeFileSync('./src/web/docs.html', html);
