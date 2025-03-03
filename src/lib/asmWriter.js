f9e831ef8ca53e003e3e1bbec4e980b56fdc22ed
Dennis Heinze <dennisjp.heinze@gmail.com>
2025-03-03 21:18:44 +0100

    Revert "remove #defines from output"
    This reverts commit 9c211da6754dd625867dcd6c2a3001675b0b1f41.

--- src/lib/asmWriter.js
+++ src/lib/asmWriter.js
@@ -255,6 +255,12 @@ export function writeASM(ast, functionsAsm, config)
 
   writeLine("## Auto-generated file, transpiled with RSPL");
 
+  if(ast.defines) {
+    for(const [name, def] of Object.entries(ast.defines)) {
+      writeLine(`#define ${name} ${def.value}`);
+    }
+  }
+
   const preIncs = generateIncs(ast.includes);
   for(const inc of preIncs)writeLine(inc);
 
