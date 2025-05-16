65fe99f29329e1a8d70e0e3cd5bfea205483fa38
Max Bebök <beboek.max@gmail.com>
2025-05-16 21:40:32 +0200

    test: better reg scan test

--- src/tests/optimizer/depScan/optRegScan.test.js
+++ src/tests/optimizer/depScan/optRegScan.test.js
@@ -1,10 +1,16 @@
 import {asm} from "../../../lib/intsructions/asmWriter.js";
 import {
-  asmGetReorderRange,
+  asmGetReorderRange, asmInitDep,
   asmInitDeps,
   getSourceRegs,
   getSourceRegsFiltered, getTargetRegs
 } from "../../../lib/optimizer/asmScanDeps.js";
+
+const laneIds = ['_0', '_1', '_2', '_3', '_4', '_5', '_6', '_7'];
+function allLanes(reg)
+{
+  return laneIds.map((id) => reg + id);
+}
 
 describe('Optimizer - Register Scanner', () =>
 {
@@ -13,33 +19,56 @@ describe('Optimizer - Register Scanner', () =>
       asm: asm("or", ["$t0", "$a1", "$a0"]),
       src: ["$a1", "$a0"],
       tgt: ["$t0"],
+      srcStall: ["$a1", "$a0"],
+      tgtStall: ["$t0"],
     },
     "Arith": {
       asm: asm("addiu", ["$t0", "$t1", 4]),
       src: ["$t1"],
       tgt: ["$t0"],
+      srcStall: ["$t1"],
+      tgtStall: ["$t0"],
     },
     "Vec Store": {
       asm: asm("sdv", ["$v08", 0, 16, "$s6"]),
-      src: ["$v08", "$s6"],
+      src: [...allLanes("$v08"), "$s6"],
       tgt: [],
+      srcStall: ["$v08", "$s6"],
+      tgtStall: [],
     },
     "Vec packed Store": {
       asm: asm("sfv", ["$v08", 0, "$s6"]),
-      src: ["$v08", "$s6"],
+      src: [...allLanes("$v08"), "$s6"],
       tgt: [],
+      srcStall: ["$v08", "$s6"],
+      tgtStall: [],
+    },
+    "Lanes - Vec move": {
+      asm: asm("vmov", ["$v07.e3", "$v05.e2"]),
+      src: ["$v05_2"],
+      tgt: ["$v07_3", "$acc"],
+      srcStall: ["$v05"],
+      tgtStall: ["$v07"],
     },
   };
 
-  for(const [name, {asm, src, tgt}] of Object.entries(CASES)) {
-    test(`Target - ${name}`, () => {
-      const target = getTargetRegs(asm);
-      expect(target).toEqual(tgt);
+  for(const [name, {asm, src, tgt, srcStall, tgtStall}] of Object.entries(CASES)) {
+    asmInitDep(asm);
+
+    test(`Source (Logic) - ${name}`, () => {
+      expect(asm.depsSource).toEqual(src);
     });
 
-    test(`Source - ${name}`, () => {
-      const source = getSourceRegsFiltered(asm);
-      expect(source).toEqual(src);
+    test(`Target (Logic) - ${name}`, () => {
+      expect(asm.depsTarget).toEqual(tgt);
+    });
+
+    test(`Source (Stalls) - ${name}`, () => {
+      expect(asm.depsStallSource).toEqual(srcStall);
+    });
+
+    test(`Target (Stalls) - ${name}`, () => {
+      expect(asm.depsStallTarget).toEqual(tgtStall);
     });
   }
 });
