57fce7beaeb27bc506d7279372d9d2690ed87ede
Max Bebök <beboek.max@gmail.com>
2025-05-29 20:41:23 +0200

    test: add basic compiler/RSP emu test

---
+++ src/tests/e2e/e2e.test.js
@@ -0,0 +1,20 @@
+import {describe, expect, jest, test} from '@jest/globals';
+import {compileAndCreateEmu} from "../helper/compileAndEmu.js";
+
+describe('E2E', () =>
+{
+  // @TODO: write proper tests, add basic armips support
+  test('Test', async () =>
+  {
+    const rsp = await compileAndCreateEmu(`
+      function main() {
+         vec16<$v02> b = 2;
+         b.x = 4;
+         b *= 2;
+    }`);
+
+    expect(rsp.getVPR("$v02")).toEqual([0,0,0,0, 0,0,0,0]);
+    rsp.step(3);
+    expect(rsp.getVPR("$v02")).toEqual([8,4,4,4, 4,4,4,4]);
+  });
+});
